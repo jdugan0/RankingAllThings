3291cfef7fc0e818e0bfc6407b6c1c1f3f40310e
jdugan0 <justin.d.dugan@gmail.com>
2026-06-27 20:51:50 +0000

    pick 2 at random

---
+++ pick2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import random
+data = pd.read_csv("data/kept.csv")
+
+i1 = random.randint(0, len(data.index) - 1)
+i2 = random.randint(0, len(data.index) - 1)
+
+row1 = data.iloc[i1]
+row2 = data.iloc[i2]
+
+print(f"\n{row1["itemLabel"]} | {row1["itemDesc"]}\n{row2["itemLabel"]} | {row2["itemDesc"]}\n")
