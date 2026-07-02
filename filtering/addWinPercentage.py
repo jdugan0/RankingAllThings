fca72922d3ba264263a3d21f998bade213c67fd1
jdugan0 <justin.d.dugan@gmail.com>
2026-07-02 13:21:57 -0400

    fix python

--- filtering/addWinPercentage.py
+++ filtering/addWinPercentage.py
@@ -2,6 +2,9 @@ import sqlite3, pandas as pd, re
 
 con = sqlite3.connect("data/things.db")
 
+con.execute("ALTER TABLE objects ADD wins INT DEFAULT 0")
+con.execute("ALTER TABLE objects ADD total INT DEFAULT 0")
+
 games = con.execute("SELECT winner_id, loser_id FROM votes").fetchall()
 for row in games:
     con.execute("UPDATE objects SET total = total + 1 WHERE id = ? OR id = ?", (int(row[0]), int(row[1])))
